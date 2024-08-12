8b3957560dc8e15941e36e58d9306ce5eacb0c93
Andy Opazo <aopazog@fen.uchile.cl>
2024-08-11 23:20:25 -0400

    hito2

--- hito1-pizzeria/src/App.jsx
+++ hito1-pizzeria/src/App.jsx
@@ -1,14 +1,19 @@
+
 import React from 'react';
 import Navbar from './components/NavbarComponent';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import RegisterPage from './components/RegisterPage';
+import LoginPage from './components/LoginPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <Navbar/>
+      {/* <Home/> */}
+      <RegisterPage />
+      {/* <LoginPage /> */}
       <Footer />
     </div>
   );
